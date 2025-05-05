1f3f17150bac7a2cb55126e8db52b97f201ed7ef
lauroolim <lauroolimota743@gmail.com>
2025-05-04 21:04:24 -0300

    feat(books): ejs form to add new books

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,5 +1,5 @@
 import { Book } from "../model/book";
-import { Response, Request } from "express";
+import express, { Request, Response } from "express";
 import { pool } from "../../db";
 
 export class BookController {
@@ -18,6 +18,66 @@ export class BookController {
       res.status(500).render("error", {
         message:
           "Erro interno do servidor ao renderizar livros, tente mais tarde...",
+      });
+    }
+  }
+
+  async displayFormAddBook(req: Request, res: Response) {
+    try {
+      res.render("books/add");
+    } catch (err) {
+      console.error("erro no display do formulario de adicionar livros" + err);
+      res.status(500).render("error", {
+        message:
+          "Erro interno do servidor ao renderizar formulario de adicionar livros, tente mais tarde...",
+      });
+    }
+  }
+
+  async displayOneBook(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      if (!id || isNaN(Number(id))) {
+        return res.status(400).render("error", {
+          message: "ID nao indentificado ou invalido",
+        });
+      }
+
+      const books = await this.book.getOne(Number(id));
+
+      if (!books) {
+        return res.status(404).render("error", {
+          message: "Livro nao encontrado",
+        });
+      }
+
+      res.render("books/detail", { books });
+    } catch (err) {
+      console.error("erro no display dos livros" + err);
+      res.status(500).render("error", {
+        message:
+          "Erro interno do servidor ao renderizar livros, tente mais tarde...",
+      });
+    }
+  }
+
+  async insertBooks(req: Request, res: Response) {
+    try {
+      if(!req.body){
+        return res.status(400).render("error", {
+          message: "Nenhum dado foi recebido",
+        });
+      }
+      const { title, author } = req.body;
+
+      const insert = await this.book.create(title, author);
+      res.redirect("/books");
+    } catch (err) {
+      console.error("falha ao inserir livro " + err);
+      res.sendStatus(500).render("error", {
+        message:
+          "Erro interno do servidor ao inserir livros, tente mais tarde...",
       });
     }
   }
--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -4,6 +4,10 @@ import { BookController } from "../controllers/books.controller";
 const rt = Router();
 const bkcontroller = new BookController();
 
-rt.get("/", (req, res) => bkcontroller.displayBooks(req, res));
+rt.get("", (req, res) => bkcontroller.displayBooks(req, res));
+rt.get("/form/add", (req, res) => bkcontroller.displayFormAddBook(req, res));
+rt.get("/:id", (req, res) => bkcontroller.displayOneBook(req, res));
+rt.post("", (req, res) => bkcontroller.insertBooks(req, res));
+
 
 export default rt;
